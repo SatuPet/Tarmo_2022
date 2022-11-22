f6ac95fd433775cf8b7410d2cd9df0186de5fcfe
SatuPet <satupet@metropolia.fi>
2022-11-22 12:28:31 +0200

    responsiivisuus ja parannuksia

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 document.getElementById("info-button").addEventListener("click", function () {
     document.getElementById('aside-window').classList.toggle('aside-open');
 
+    // Info text change to sulje
+    if (this.innerText == "Info") {
+        this.innerText = "Sulje";
+      } else {
+        this.innerText = "Info";
+      }
+
     // Open info content with timing
     let infoContent = document.getElementById('info-content');
 
@@ -26,6 +33,35 @@ if (!controls.classList.contains('controls-hidden')) {
 }
 
 });
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
 
 
 
